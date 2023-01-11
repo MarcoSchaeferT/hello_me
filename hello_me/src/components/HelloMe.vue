c51e8c3448bd22188ddc2ac0a21ee18b6e29d5ce
MarcoSchaeferT <marco.schaefer@uni-tuebingen.de>
2023-01-11 13:38:16 +0100

    added npm commands for dev & production bulid

--- hello_me/src/components/HelloMe.vue
+++ hello_me/src/components/HelloMe.vue
@@ -1,13 +1,20 @@
 <template>
-    <v-card class="mx-auto" rounded="2">
-        <v-img height="100%" cover src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg">
-            <v-avatar image="@/assets/marco.png" size="150" rounded="0">
-            </v-avatar>
-            <v-list-item class="text-white" title="Marco Schäfer" subtitle="Visualization Programmer"></v-list-item>
-        </v-img>
-    </v-card>
+  <v-card class="mx-auto" rounded="2">
+    <v-img
+      height="100%"
+      cover
+      src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
+    >
+      <v-avatar image="@/assets/marco.png" size="150" rounded="0"> </v-avatar>
+      <v-list-item
+        class="text-white"
+        title="Marco Schäfer"
+        subtitle="Visualization Programmer"
+      ></v-list-item>
+    </v-img>
+  </v-card>
 </template>
 
 <script lang="ts" setup>
-  //
+//
 </script>
